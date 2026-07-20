561aa09e5c881f3a444faedb3757d470ab1afda2
Ronish27-meet <roni.shuker.shenhar@gmail.com>
2026-07-20 15:58:01 +0300

    main

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+from anthropic import Anthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Anthropic(api_key=os.getenv('ANTHROPIC_API_KEY'))
+
+print("do you want to use agent 1 or 2?")
