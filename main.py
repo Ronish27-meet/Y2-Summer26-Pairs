7248ea5db1b7a60cb2e980c40e2542721b0d0148
mohamedkh27-meet <mohakhaldi1999@gmail.com>
2026-07-20 17:53:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,17 @@ from app2 import run_chat as roey
 
 load_dotenv()
 
-client = Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
-
 print("Hello! If you're feeling sad, this app gives you two options.")
 print("1 - Emotional eater")
 print("2 - Talk with a therapist")
 
-
 while True:
-    choice = input("What do you want to choose? (1/2): ")
+    choice = input("What do you want to choose? (1/2) or exit the app by writing -exit-: ")
     if choice == "1":
         coco()
     elif choice == "2":
         roey()
+    elif choice.lower() == "exit":
+        break 
     else:
         print("Please enter 1 or 2.")
